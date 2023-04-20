9ae09415c7129103d052b9f638bb959f390b68d1
loic <loic@allsoftware.fr>
2023-04-20 14:59:14 +0200

    Configuration XDebug compatible PHP 8.x (y compris PHP CLI)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Image symfony for docker
+
+- docker login
+- docker build -t allsoftware/symfony:[nouveau TAG] .
+- docker push allsoftware/symfony:[nouveau TAG]
--- xdebug_state.sh
+++ xdebug_state.sh
@@ -17,10 +17,19 @@ if [ $XDEBUG_ENABLE -eq 1 ]; then
   echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini \
       && echo "xdebug.idekey=PHPSTORM" >> /usr/local/etc/php/conf.d/xdebug.ini \
       && echo "xdebug.mode=debug" >> /usr/local/etc/php/conf.d/xdebug.ini \
-      && echo "xdebug.discover_client_host=1" >> /usr/local/etc/php/conf.d/xdebug.ini \
-      && echo "xdebug.client_host.=$XDEBUG_REMOTE_MACHINE" >> /usr/local/etc/php/conf.d/xdebug.ini \
+      && echo "xdebug.discover_client_host=0" >> /usr/local/etc/php/conf.d/xdebug.ini \
+      && echo "xdebug.client_host=$XDEBUG_REMOTE_MACHINE" >> /usr/local/etc/php/conf.d/xdebug.ini \
       && echo "xdebug.client_port=$XDEBUG_REMOTE_PORT_MACHINE" >> /usr/local/etc/php/conf.d/xdebug.ini \
-      && echo "xdebug.log='/tmp/xdebug.log'" >> /usr/local/etc/php/conf.d/xdebug.ini
+      && echo "xdebug.log='/tmp/xdebug.log'" >> /usr/local/etc/php/conf.d/xdebug.ini \
+      && echo "xdebug.log_level=3" >> /usr/local/etc/php/conf.d/xdebug.ini
+
+  read -p "Avec debug en mode CLI (1) ou non (vide) ? : " XDEBUG_REMOTE_AVEC_CLI
+  if [ -z $XDEBUG_REMOTE_AVEC_CLI ]; then
+     unset XDEBUG_CONFIG
+  else
+     export XDEBUG_CONFIG=""
+  fi
+
 else
   echo "" > /usr/local/etc/php/conf.d/xdebug.ini
 fi
